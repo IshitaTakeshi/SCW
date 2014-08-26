c1d0ee1080dc11e1def9a72456e0dc39bbcd8536
Takeshi Ishita <ishitah.takeshi@gmail.com>
2014-08-26 21:51:55 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+from distutils.core import setup
+
+setup(
+    name='scw',
+    version='1.0',
+    description='Soft Confidence Weighted Learning', 
+    py_modules=['scw'],
+    author='Ishita Takeshi',
+)

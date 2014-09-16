b0cf8557419a81c84c2e932aba15b1bca9b69b10
Takeshi Ishita <ishitah.takeshi@gmail.com>
2014-09-16 14:24:54 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 import os
 from setuptools import setup
 
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
 setup(
     name='scw',
     version='1.0',
-    description='Soft Confidence Weighted Learning',
     author='Ishita Takeshi',
+    description=("An implementation of " 
+                 "Exact Soft Confidence-Weighted Learning"),
+    license='MIT',
+    keywords = "Exact Soft Confidence-Weighted Learning",
+    url='https://pypi.python.org/pypi/scw',
+    long_description=read('README.md'),
+    author_email='ishitah.takeshi@gmail.com'
 )

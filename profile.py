b04acc1f0aee629dd7e5576422c0648aca1ba478
sonicair <ishitah.takeshi@gmail.com>
2016-02-21 19:08:36 +0900

    Implement profiling of training time and accuracy

---
+++ profile.py
@@ -0,0 +1,35 @@
+import time
+
+from sklearn import datasets
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.svm import LinearSVC
+
+from scw import SCW1, SCW2
+
+
+def generate_dataset():
+    X, y = datasets.make_classification(n_samples=5000, n_features=10)
+    data = train_test_split(X, y)
+    training = data[0], data[2]
+    test = data[1], data[3]
+    return training, test
+
+
+def profile(model, training, test):
+    X, y = training
+
+    t1 = time.time()
+    model.fit(X, y)
+    t2 = time.time()
+    print("spent time for training :  {}".format(t2-t1))
+
+    X, y_true = test
+    y_pred = model.predict(X)
+    accuracy = accuracy_score(y_pred, y_true)
+    print("accuracy :  {}".format(accuracy))
+
+
+training, test = generate_dataset()
+profile(SCW1(), training, test)
+profile(LinearSVC(), training, test)

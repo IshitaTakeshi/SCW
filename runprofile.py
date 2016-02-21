56836f24aeb429d63b02089e7697a4869141db2d
sonicair <ishitah.takeshi@gmail.com>
2016-02-22 02:25:20 +0900

    Move dataset generator to dataset.py

--- runprofile.py
+++ runprofile.py
@@ -4,23 +4,13 @@ import pstats
 
 import numpy as np
 
-from sklearn import datasets
-from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.svm import LinearSVC
 
 import utils
+import datasets
 from scw import SCW1, SCW2
 
-
-def generate_dataset():
-    X, y = datasets.make_classification(n_samples=5000, n_features=10,
-                                        n_classes=2)
-    y = utils.overwrite_labels(y)  # (0, 1) to (-1, 1)
-    data = train_test_split(X, y)
-    training = data[0], data[2]
-    test = data[1], data[3]
-    return training, test
 
 
 def accuracy_and_time(model, model_name, training, test):
@@ -39,8 +29,7 @@ def accuracy_and_time(model, model_name, training, test):
     print("    Accuracy :  {}\n".format(accuracy))
 
 
-def run_profile(model, model_name, data):
-    X, y = data
+def run_profile(model, model_name, X, y):
     filename = model_name+".def"
     profile.runctx("model.fit(X, y)", globals(), locals(), filename)
 
@@ -48,11 +37,13 @@ def run_profile(model, model_name, data):
     #p.print_stats()
 
 
-training, test = generate_dataset()
 
+X, y = datasets.make_classification()
+training, test = utils.train_test_split(X, y)
 accuracy_and_time(SCW1(), "SCW1", training, test)
 accuracy_and_time(SCW2(), "SCW2", training, test)
 accuracy_and_time(LinearSVC(), "LinearSVC", training, test)
 
-run_profile(SCW1(), "SCW1", training)
-run_profile(SCW2(), "SCW2", training)
+X, y = datasets.load_digits()
+run_profile(SCW1(), "SCW1", X, y)
+run_profile(SCW2(), "SCW2", X, y)

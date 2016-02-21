fa01bae61830e501e62997f456f9533b654eb425
sonicair <ishitah.takeshi@gmail.com>
2016-02-22 02:21:51 +0900

    Add data splitter for cross validation

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn import cross_validation
 
 
 def overwrite_labels(y):
@@ -6,3 +7,10 @@ def overwrite_labels(y):
     y[y==classes[0]] = -1
     y[y==classes[1]] = 1
     return y
+
+
+def train_test_split(X, y, test_size=0.2):
+    data = cross_validation.train_test_split(X, y, test_size=test_size)
+    training = data[0], data[2]
+    test = data[1], data[3]
+    return training, test
